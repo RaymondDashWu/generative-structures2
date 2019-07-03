0b034a2fd070955dabea042de312e618dc9ab5de
RaymondDashWu <raymond31670@hotmail.com>
2019-07-03 00:12:53 -0700

    [ADD] Explainer tests

--- generative_structures/explainer/tests.py
+++ generative_structures/explainer/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from django.contrib.staticfiles import finders
+
+class TestPages(TestCase):    
+    def test_get_homepage(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_find_static_file(self):
+        self.assertNotEqual(finders.find('~/static/index.html'), 'None')
+        self.assertNotEqual(finders.find('~/static/img/bg-masthead.webp'), 'None')
+        self.assertNotEqual(finders.find('~/static/css/creative.min,css'), 'None')
--- generative_structures/openjscad/tests.py
+++ generative_structures/openjscad/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class TestPages(TestCase):        
+    # This now page hosts the client side js + dapps because it wasn't possible to do what I wanted to in Django
+    def test_get_openjscad(self):
+        c = Client()
+        response = c.get('https://web.raymond31670.now.sh/')
+        self.assertEqual(response.status_code, 200)
