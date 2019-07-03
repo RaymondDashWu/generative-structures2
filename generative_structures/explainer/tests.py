7f63b7efbe7232d2f4c83131071a57d897589bbd
RaymondDashWu <raymond31670@hotmail.com>
2019-07-03 00:20:58 -0700

    [FIX] Explainer tests

--- generative_structures/explainer/tests.py
+++ generative_structures/explainer/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-
 from django.contrib.staticfiles import finders
 
 class TestPages(TestCase):    
@@ -10,6 +9,6 @@ class TestPages(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_find_static_file(self):
-        self.assertNotEqual(finders.find('~/static/index.html'), 'None')
-        self.assertNotEqual(finders.find('~/static/img/bg-masthead.webp'), 'None')
-        self.assertNotEqual(finders.find('~/static/css/creative.min,css'), 'None')
+        self.assertNotEqual(finders.find('../static/index.html'), None)
+        self.assertNotEqual(finders.find('../static/img/bg-masthead.webp'), None)
+        self.assertNotEqual(finders.find('../static/css/creative.min.css'), None)
--- generative_structures/openjscad/tests.py
+++ generative_structures/openjscad/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from django.contrib.staticfiles import finders
 
 class TestPages(TestCase):        
     # This now page hosts the client side js + dapps because it wasn't possible to do what I wanted to in Django
@@ -6,3 +7,9 @@ class TestPages(TestCase):
         c = Client()
         response = c.get('https://web.raymond31670.now.sh/')
         self.assertEqual(response.status_code, 200)
+
+    def test_find_static_file(self):
+        # self.assertNotEqual(finders.find('~/contracts/generate.sol.html'), None)
+        # self.assertNotEqual(finders.find('~/static/img/bg-maskdljsafjlkasthead.webp'), None)
+        # self.assertNotEqual(finders.find('~/static/css/creative.min,css'), None)
+        pass
